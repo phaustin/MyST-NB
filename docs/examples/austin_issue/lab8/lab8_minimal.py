bcbc8a6a53ca292eb9cc46cf9382e8610e81f198
Phil Austin <paustin@eos.ubc.ca>
2020-04-01 08:49:16 -0700

    minimal notebook

---
+++ docs/examples/austin_issue/lab8/lab8_minimal.py
@@ -0,0 +1,13 @@
+# %% [markdown]
+# # two test equations
+
+# %% [markdown]
+# \begin{equation}
+# \frac{\partial v}{\partial t} + u \frac{\partial v}{\partial x} + v \frac{\partial v}{\partial y} + w \frac{\partial v}{\partial z} + fu = - \, \frac{1}{\rho} \, \frac{\partial p}{\partial y}
+# + A_v \, \frac{\partial^2 v}{\partial z^2} + A_h \, \nabla^2 v
+# \end{equation}
+
+# %% [markdown]
+# \begin{equation}
+# \frac {\partial u}{\partial x} + \frac{\partial v}{\partial y} = - \, \frac{\partial w}{\partial z}
+# \end{equation}
