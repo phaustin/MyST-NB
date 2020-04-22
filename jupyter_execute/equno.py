ce9cd431b939ff275fd1ea91585c10082e36dfa8
Phil Austin <paustin@eos.ubc.ca>
2020-04-21 18:36:49 -0700

    Update documentation

--- jupyter_execute/equno.py
+++ jupyter_execute/equno.py
@@ -1,31 +1,7 @@
-import numpy as np
-3.8*np.log(890/280)/np.log(2)
-
 # Equation test
 
-## Delimiters
+## Source -- no $$
 
-### Source -- with $$
-```
-$$
-\begin{align}
-\dot{x} & = \sigma(y-x) \\
-\dot{y} & = \rho x - y - xz \\
-\dot{z} & = -\beta z + xy
-\end{align}
-$$(eq:one)
-```
-### Display \$\$
-
-$$
-\begin{align}
-\dot{x} & = \sigma(y-x) \\
-\dot{y} & = \rho x - y - xz \\
-\dot{z} & = -\beta z + xy
-\end{align}
-$$(eq:one)
-
-### Source -- no \$\$
 ```
 \begin{align*}
 \dot{x} & = \sigma(y-x) \\
@@ -33,7 +9,7 @@ $$(eq:one)
 \dot{z} & = -\beta z + xy
 \end{align*}
 ```
-### Display no $$
+## Display no $$
 
 
 \begin{align*}
@@ -41,52 +17,3 @@ $$(eq:one)
 \dot{y} & = \rho x - y - xz \\
 \dot{z} & = -\beta z + xy
 \end{align*}
-
-Reference {eq}`eq:one`
-
-## aligned equations
-
-### align numbering single
-
-```
-$$
-\begin{align} 
-g(X_{n}) &= g(\theta)+g'({\tilde{\theta}})(X_{n}-\theta) \notag \\
-\sqrt{n}[g(X_{n})-g(\theta)] &= g'\left({\tilde{\theta}}\right)
-  \sqrt{n}[X_{n}-\theta ] 
-\end{align} 
-$$(eq:align1)
-```
-
-$$
-\begin{align} 
-g(X_{n}) &= g(\theta)+g'({\tilde{\theta}})(X_{n}-\theta) \notag \\
-\sqrt{n}[g(X_{n})-g(\theta)] &= g'\left({\tilde{\theta}}\right)
-  \sqrt{n}[X_{n}-\theta ] 
-\end{align} 
-$$(eq:align1)
-
-### align numbering multiple
-
-```
-$$
-\begin{align}
-\mathrm{Var}(\hat{\beta}) & =\mathrm{Var}((X'X)^{-1}X'y)\\
- & =(X'X)^{-1}X'\mathrm{Var}(y)((X'X)^{-1}X')'  \tag{subtest1}\\
- & =(X'X)^{-1}X'\mathrm{Var}(y)X(X'X)^{-1}\notag\\
- & =(X'X)^{-1}X'\sigma^{2}IX(X'X)^{-1}\\
- & =(X'X)^{-1}\sigma^{2}\tag{subtest2}
-\end{align}
- $$
-```
-
-
-$$
-\begin{align}
-\mathrm{Var}(\hat{\beta}) & =\mathrm{Var}((X'X)^{-1}X'y)\\
- & =(X'X)^{-1}X'\mathrm{Var}(y)((X'X)^{-1}X')'  \tag{subtest1}\\
- & =(X'X)^{-1}X'\mathrm{Var}(y)X(X'X)^{-1}\notag\\
- & =(X'X)^{-1}X'\sigma^{2}IX(X'X)^{-1}\\
- & =(X'X)^{-1}\sigma^{2}\tag{subtest2}
- \end{align}
- $$
