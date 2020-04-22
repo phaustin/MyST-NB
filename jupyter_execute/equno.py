f5e7dfddc0eba31ba71a361809204dfb03d9c918
Phil Austin <paustin@eos.ubc.ca>
2020-04-21 18:56:35 -0700

    Update documentation

---
+++ jupyter_execute/equno.py
@@ -0,0 +1,29 @@
+# Equation test
+
+## Source -- no $$
+
+```
+\begin{align*}
+\dot{x} & = \sigma(y-x) \\
+\dot{y} & = \rho x - y - xz \\
+\dot{z} & = -\beta z + xy
+\end{align*}
+```
+
+## first without, then with $$
+
+
+\begin{align*}
+\dot{x} & = \sigma(y-x) \\
+\dot{y} & = \rho x - y - xz \\
+\dot{z} & = -\beta z + xy
+\end{align*}
+
+$$
+\begin{align*}
+\dot{x} & = \sigma(y-x) \\
+\dot{y} & = \rho x - y - xz \\
+\dot{z} & = -\beta z + xy
+\end{align*}
+$$
+
